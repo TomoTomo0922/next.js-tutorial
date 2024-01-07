f9aca5343611d856642a61be3b12c5330cba0abd
TomoTomo000 <tomoyuki.oneokrock69@gmail.com>
2024-01-07 19:09:09 +0900

    初期コミット

---
+++ pages/index.tsx
@@ -0,0 +1,54 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Date from '../components/date';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+import { getSortedPostsData } from '../lib/posts';
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+type Props = {
+  allPostsData: {
+    id:string
+    title:string
+    date:string
+  }[]
+}
+
+export default function Home({ allPostsData } : Props) {
+  return (
+    <>
+    <Head>
+      <title>ともブログ</title>
+    </Head>
+    <Layout home>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+    </>
+  );
+}
